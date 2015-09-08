5e69cb9ae61d480966e18db739ac9edc437cac0d
joshjung <joshua.p.jung@gmail.com>
2015-09-08 11:27:45 -0500

    Initial Angular setup.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', [])
+  .controller('controllerRoot', function ($scope) {
+    $scope.text = "Hello World";
+
+    $scope.calculate = function (amount) {
+      if (isNaN(parseFloat(amount)))
+        return 'Enter a number';
+      return Math.sqrt(amount);
+    }
+  });
