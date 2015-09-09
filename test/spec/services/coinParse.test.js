a696c71b5a79c5c3cb4b49a9a18566d71fc06b57
joshjung <joshua.p.jung@gmail.com>
2015-09-09 09:04:16 -0500

    Fixing test for parser.

--- test/spec/services/coinParse.test.js
+++ test/spec/services/coinParse.test.js
@@ -30,7 +30,7 @@ describe('Services: CoinParseService', function () {
   });
 
   it('parse should properly error 500.5', function () {
-    expect(CoinParseService.parse('500.5')).toBe('Please provide a denomination.');
+    expect(CoinParseService.parse('500.5')).toBe(500.5);
   });
 
   it('parse should properly error ASDF', function () {
