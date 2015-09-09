db492221e9e586c28999619a6ef0e2bcbfd066b4
joshjung <joshua.p.jung@gmail.com>
2015-09-08 22:38:06 -0500

    Fixed the coin parse service and got unit tests for it.

--- app/scripts/services/coinParse.service.js
+++ app/scripts/services/coinParse.service.js
@@ -2,5 +2,31 @@
 
 angular.module('appCoinReturn')
   .service('CoinParseService', function() {
-    this.test = true;
+    this.parse = function (value) {
+      var lbs = /^£/i.test(value),
+          pence = /p$/i.test(value);
+
+      if (lbs && pence)
+      {
+        return 'Please do not provide lbs and pence.';
+      }
+      else if (!(lbs || pence)) // DeMorgan's
+      {
+        return 'Please provide a denomination.';
+      }
+
+      if (lbs)
+      {
+        return parseFloat(value.slice(1)) * 100;
+      }
+
+      var val = parseFloat(value);
+
+      if (isNaN(val))
+      {
+        return 'Please provide a valid imperial currency.';
+      }
+
+      return val;
+    };
   });
--- test/spec/services/coinParse.test.js
+++ test/spec/services/coinParse.test.js
@@ -9,7 +9,31 @@ describe('Services: CoinParseService', function () {
     CoinParseService = _CoinParseService_;
   }));
 
-  it('parse should properly convert input text to pence', function () {
-    expect(CoinParseService.test).toBe(true);
+  it('parse should properly evaluate 99p', function () {
+    expect(CoinParseService.parse('99p')).toBe(99);
+  });
+
+  it('parse should properly evaluate 200p', function () {
+    expect(CoinParseService.parse('200p')).toBe(200);
+  });
+
+  it('parse should properly evaluate £1', function () {
+    expect(CoinParseService.parse('£1')).toBe(100);
+  });
+
+  it('parse should properly evaluate £500.5', function () {
+    expect(CoinParseService.parse('£500.5')).toBe(50050);
+  });
+
+  it('parse should properly error £500.5p', function () {
+    expect(CoinParseService.parse('£500.5p')).toBe('Please do not provide lbs and pence.');
+  });
+
+  it('parse should properly error 500.5', function () {
+    expect(CoinParseService.parse('500.5')).toBe('Please provide a denomination.');
+  });
+
+  it('parse should properly error ASDF', function () {
+    expect(CoinParseService.parse('ASDFp')).toBe('Please provide a valid imperial currency.');
   });
 });
